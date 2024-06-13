d25cb6d020172869235cfb88130c36cf6cde0e46
Hippolyte Gander <hippolyte.gander5@gmail.com>
2024-06-13 16:55:34 +0200

    début ajout form add Stagiaire (incomplet)

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Stagiaire;
 use App\Repository\StagiaireRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
@@ -15,6 +18,35 @@ class StagiaireController extends AbstractController
         $stagiaires = $stagiaireRepository->findAll();
         return $this->render('stagiaire/index.html.twig', [
             'stagiaires' => $stagiaires
+        ]);
+    }
+
+    #[Route('/stagiaire/new', name: 'new_stagiaire')]
+    #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+    public function new(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager): Response
+    { 
+        if (!$stagiaire){
+            $stagiaire = new Stagiaire();
+        }
+
+        $form =$this->createForm(StagiaireType::class, $stagiaire);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $stagiaire = $form->getData();
+            // prepare en PDO
+            $entityManager->persist($stagiaire);
+            // execute en PDO
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stagiaire');
+        }
+
+        return $this->render('stagiaire/new.html.twig', [
+            'formAddStagiaire' => $form,
+            'edit' => $stagiaire->getId()
         ]);
     }
 }
